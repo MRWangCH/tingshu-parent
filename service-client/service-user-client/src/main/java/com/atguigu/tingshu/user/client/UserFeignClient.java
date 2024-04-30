24d42d9ce103225b7c452f256d0417453b1e1e02
wangchuan <1016368304@qq.com>
2024-04-30 12:35:24 +0800

    专辑详情模块：用户微服务提供feign接口查询专辑以及声音购买情况

--- service-client/service-user-client/src/main/java/com/atguigu/tingshu/user/client/UserFeignClient.java
+++ service-client/service-user-client/src/main/java/com/atguigu/tingshu/user/client/UserFeignClient.java
@@ -6,6 +6,11 @@ import com.atguigu.tingshu.vo.user.UserInfoVo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -23,5 +28,15 @@ public interface UserFeignClient {
      */
     @GetMapping("/userInfo/getUserInfoVo/{userId}")
     public Result<UserInfoVo> getUserInfoVoByUserId(@PathVariable Long userId);
+
+    /**
+     * 获取专辑声音列表某页中，用户对于声音的付费情况
+     * @param userId
+     * @param albumId
+     * @param trackIdList
+     * @return
+     */
+    @PostMapping("/userInfo/userIsPaidTrack/{userId}/{albumId}")
+    public Result<Map<Long, Integer>> userIsPaidTrackList(@PathVariable("userId") Long userId, @PathVariable("albumId") Long albumId, @RequestBody List<Long> trackIdList);
 
 }
--- service-client/service-user-client/src/main/java/com/atguigu/tingshu/user/client/impl/UserDegradeFeignClient.java
+++ service-client/service-user-client/src/main/java/com/atguigu/tingshu/user/client/impl/UserDegradeFeignClient.java
@@ -7,6 +7,9 @@ import com.atguigu.tingshu.vo.user.UserInfoVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+
 @Slf4j
 @Component
 public class UserDegradeFeignClient implements UserFeignClient {
@@ -14,6 +17,12 @@ public class UserDegradeFeignClient implements UserFeignClient {
     @Override
     public Result<UserInfoVo> getUserInfoVoByUserId(Long userId) {
         log.error("远程调用[用户服务getUserInfoVoByUserId方法服务降级]");
+        return null;
+    }
+
+    @Override
+    public Result<Map<Long, Integer>> userIsPaidTrackList(Long userId, Long albumId, List<Long> trackIdList) {
+        log.error("远程调用[用户服务userIsPaidTrackList方法服务降级]");
         return null;
     }
 }
