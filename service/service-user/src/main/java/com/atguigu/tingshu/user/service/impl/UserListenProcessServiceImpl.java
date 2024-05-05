acc8ca8e3766f09dd523e40b72b3b9e4c379376a
wangchuan <1016368304@qq.com>
2024-05-05 17:36:05 +0800

    专辑详情模块：使用Kafka实现异步更新专辑/声音统计信息，防止恶意刷播放量，采用redis设置key的过期时间

--- service/service-user/src/main/java/com/atguigu/tingshu/user/service/impl/UserListenProcessServiceImpl.java
+++ service/service-user/src/main/java/com/atguigu/tingshu/user/service/impl/UserListenProcessServiceImpl.java
@@ -1,19 +1,28 @@
 package com.atguigu.tingshu.user.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.IdUtil;
+import com.alibaba.fastjson.JSON;
+import com.atguigu.tingshu.common.constant.KafkaConstant;
+import com.atguigu.tingshu.common.constant.RedisConstant;
+import com.atguigu.tingshu.common.constant.SystemConstant;
+import com.atguigu.tingshu.common.service.KafkaService;
 import com.atguigu.tingshu.common.util.AuthContextHolder;
 import com.atguigu.tingshu.common.util.MongoUtil;
 import com.atguigu.tingshu.model.user.UserListenProcess;
 import com.atguigu.tingshu.user.service.UserListenProcessService;
+import com.atguigu.tingshu.vo.album.TrackStatMqVo;
 import com.atguigu.tingshu.vo.user.UserListenProcessVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @SuppressWarnings({"all"})
@@ -21,6 +30,12 @@ public class UserListenProcessServiceImpl implements UserListenProcessService {
 
     @Autowired
     private MongoTemplate mongoTemplate;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private KafkaService kafkaService;
 
     /**
      * 查询当前用户入参声音上次播放的进度
@@ -72,6 +87,20 @@ public class UserListenProcessServiceImpl implements UserListenProcessService {
             mongoTemplate.save(listenProcess, collectionName);
         }
 
-        //4 TODO 基于Kafka异步通知声音/专辑的统计信息
+        //4 基于Kafka异步通知声音/专辑的统计信息
+        //4.1 避免用户恶意刷播放量，一天同一用户对同一声音只能统计一次
+        String key = RedisConstant.USER_TRACK_REPEAT_STAT_PREFIX + userId + ":" + userListenProcessVo.getTrackId();
+        Boolean flag = redisTemplate.opsForValue().setIfAbsent(key, userId, 24, TimeUnit.HOURS);
+        //4.2 封装更新统计信息的MQ对象，将发送到”声音统计“的topic中，由专辑微服务作为消费者监听更新声音专辑统计信息
+        if (flag) {
+            TrackStatMqVo mqVo = new TrackStatMqVo();
+            //为了避免mq服务器对同一个消息进行多次投递，产生业务标识
+            mqVo.setBusinessNo(RedisConstant.BUSINESS_PREFIX + IdUtil.fastSimpleUUID());
+            mqVo.setStatType(SystemConstant.TRACK_STAT_PLAY);
+            mqVo.setAlbumId(userListenProcessVo.getAlbumId());
+            mqVo.setTrackId(userListenProcessVo.getTrackId());
+            mqVo.setCount(1);
+            kafkaService.sendMessage(KafkaConstant.QUEUE_TRACK_STAT_UPDATE, JSON.toJSONString(mqVo));
+        }
     }
 }
