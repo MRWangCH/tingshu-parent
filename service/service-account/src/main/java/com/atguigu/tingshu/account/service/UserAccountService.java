a8575c1bcba444c2c37589b0838ad1164c040eac
wangchuan <1016368304@qq.com>
2024-05-12 15:01:34 +0800

    订单模块：获取账户以及账户可用余额

--- service/service-account/src/main/java/com/atguigu/tingshu/account/service/UserAccountService.java
+++ service/service-account/src/main/java/com/atguigu/tingshu/account/service/UserAccountService.java
@@ -3,6 +3,8 @@ package com.atguigu.tingshu.account.service;
 import com.atguigu.tingshu.model.account.UserAccount;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.math.BigDecimal;
+
 public interface UserAccountService extends IService<UserAccount> {
 
     /**
@@ -11,4 +13,17 @@ public interface UserAccountService extends IService<UserAccount> {
      * @param userId
      */
     void saveUserAccount(Long userId);
+
+    /**
+     * 获取当前登录用户可用余额
+     * @return
+     */
+    BigDecimal getAvailableAmount(Long userId);
+
+    /**
+     * 查询账户信息
+     * @param userId
+     * @return
+     */
+    UserAccount getUserAccount(Long userId);
 }
--- service/service-account/src/main/java/com/atguigu/tingshu/account/service/impl/UserAccountServiceImpl.java
+++ service/service-account/src/main/java/com/atguigu/tingshu/account/service/impl/UserAccountServiceImpl.java
@@ -3,11 +3,14 @@ package com.atguigu.tingshu.account.service.impl;
 import com.atguigu.tingshu.account.mapper.UserAccountMapper;
 import com.atguigu.tingshu.account.service.UserAccountService;
 import com.atguigu.tingshu.model.account.UserAccount;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 @Slf4j
 @Service
@@ -28,5 +31,35 @@ public class UserAccountServiceImpl extends ServiceImpl<UserAccountMapper, UserA
         UserAccount userAccount = new UserAccount();
         userAccount.setUserId(userId);
         userAccountMapper.insert(userAccount);
+    }
+
+    /**
+     * 获取当前登录用户可用余额
+     *
+     * @return
+     */
+    @Override
+    public BigDecimal getAvailableAmount(Long userId) {
+        //1 获取账户信息
+        UserAccount userAccount = this.getUserAccount(userId);
+        //2 获取可用余额
+        if (userAccount != null) {
+            BigDecimal availableAmount = userAccount.getAvailableAmount();
+            return availableAmount;
+        }
+        return null;
+    }
+
+    /**
+     * 查询账户信息
+     *
+     * @param userId
+     * @return
+     */
+    @Override
+    public UserAccount getUserAccount(Long userId) {
+        LambdaQueryWrapper<UserAccount> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(UserAccount::getUserId, userId);
+        return userAccountMapper.selectOne(queryWrapper);
     }
 }
