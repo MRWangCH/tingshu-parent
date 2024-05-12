7d486f8f0335fa26e41e16f6c7bd6a59f988441b
wangchuan <1016368304@qq.com>
2024-05-12 15:23:43 +0800

    订单模块：获取VIP套餐信息

--- service/service-user/src/main/java/com/atguigu/tingshu/user/api/VipServiceConfigApiController.java
+++ service/service-user/src/main/java/com/atguigu/tingshu/user/api/VipServiceConfigApiController.java
@@ -1,10 +1,16 @@
 package com.atguigu.tingshu.user.api;
 
+import com.atguigu.tingshu.common.result.Result;
+import com.atguigu.tingshu.model.user.VipServiceConfig;
 import com.atguigu.tingshu.user.service.VipServiceConfigService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Tag(name = "VIP服务配置管理接口")
 @RestController
@@ -12,8 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @SuppressWarnings({"all"})
 public class VipServiceConfigApiController {
 
-	@Autowired
-	private VipServiceConfigService vipServiceConfigService;
+    @Autowired
+    private VipServiceConfigService vipServiceConfigService;
+
+    /**
+     * 获取所有vip套餐类型
+     * @return
+     */
+    @Operation(summary = "获取所有vip套餐类型")
+    @GetMapping("/vipServiceConfig/findAll")
+    public Result<List<VipServiceConfig>> getVipServiceConfig() {
+        List<VipServiceConfig> vipServiceConfigs = vipServiceConfigService.list();
+        return Result.ok(vipServiceConfigs);
+    }
 
 }
 
