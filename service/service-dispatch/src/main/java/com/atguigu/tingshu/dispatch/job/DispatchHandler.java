6eccfbf2838d49bad78199bda2c1ef74d1e49a9a
wangchuan <1016368304@qq.com>
2024-05-20 16:21:46 +0800

    定时任务模块：引入定时任务框架Xxl-job

--- service/service-dispatch/src/main/java/com/atguigu/tingshu/dispatch/job/DispatchHandler.java
+++ service/service-dispatch/src/main/java/com/atguigu/tingshu/dispatch/job/DispatchHandler.java
@@ -1,5 +1,7 @@
 package com.atguigu.tingshu.dispatch.job;
 
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -7,4 +9,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class DispatchHandler {
 
+    /**
+     * 定时执行热门专辑的更新
+     */
+    @XxlJob("updateHotAlbumJob")
+    public void updateHotAlbumJob() {
+        log.info("定时执行热门专辑的更新");
+    }
+
+
+    /**
+     * 定时执行用户vip状态的更新
+     * @return
+     */
+    @XxlJob("updateUserVIPStatusJob")
+    public ReturnT updateUserVIPStatusJob() {
+        try {
+            log.info("定时执行用户vip状态的更新");
+            return ReturnT.SUCCESS;
+        } catch (Exception e) {
+            return ReturnT.FAIL;
+        }
+    }
 }
